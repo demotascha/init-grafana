da352fd300216c9ba00cfc09b022f43325863f61
DemoHuang <demotascha@gmail.com>
2023-02-20 04:00:05 +0000

    Add folder

--- main.tf
+++ main.tf
@@ -12,12 +12,20 @@ provider "grafana" {
     auth  = "admin:gcp"
 }
 
+resource "grafana_folder" "FluentBit" {
+    title = "FluentBit"
+}
+
 resource "grafana_dashboard" "FluentBit" {
     config_json = file("./dashboards/fluent-bit.json")
-    folder      = "General"
+    folder      = grafana_folder.FluentBit.id
+}
+
+resource "grafana_folder" "SpringBootObservability" {
+    title = "SpringBootObservability"
 }
 
 resource "grafana_dashboard" "SpringBootObservability" {
     config_json = file("./dashboards/spring-boot-observability.json")
-    folder      = "General"
+    folder      = grafana_folder.SpringBootObservability.id
 }
